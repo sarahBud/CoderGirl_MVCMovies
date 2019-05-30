32ca199e391c486cd65dd0237d9b3f9aa7cfe84c
dwmoeller1 <dwmoeller1@aol.com>
2019-05-29 21:08:33 -0500

    In class work

--- CoderGirl_MVCMovies/Controllers/MovieController.cs
+++ CoderGirl_MVCMovies/Controllers/MovieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CoderGirl_MVCMovies.Data;
 using CoderGirl_MVCMovies.Models;
+using CoderGirl_MVCMovies.ViewModels.Movie;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoderGirl_MVCMovies.Controllers
@@ -22,29 +23,30 @@ namespace CoderGirl_MVCMovies.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-            ViewBag.Directors = directorRepository.GetModels().Cast<Director>().ToList();
-            return View();
+            MovieCreateViewModel model = MovieCreateViewModel.GetMovieCreateViewModel();
+            return View(model);
         }
 
         [HttpPost]
-        public IActionResult Create(Movie movie)
+        public IActionResult Create(MovieCreateViewModel model)
         {
-            if (String.IsNullOrWhiteSpace(movie.Name))
+            if (String.IsNullOrWhiteSpace(model.Name))
             {
                 ModelState.AddModelError("Name", "Name must be included");
             }
-            if(movie.Year < 1888 || movie.Year > DateTime.Now.Year)
+            if(model.Year < 1888 || model.Year > DateTime.Now.Year)
             {
                 ModelState.AddModelError("Year", "Year is not valid");
             }
 
             if(ModelState.ErrorCount > 0)
             {
-                ViewBag.Directors = directorRepository.GetModels().Cast<Director>().ToList();
-                return View(movie);
+                model.Directors = directorRepository.GetModels().Cast<Director>().ToList();
+                return View(model);
             }
 
-            movieRepository.Save(movie);
+            model.Persist();
+            //movieRepository.Save(model);
             return RedirectToAction(actionName: nameof(Index));
         }
 
---
+++ CoderGirl_MVCMovies/ViewModels/Movie/MovieCreateViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CoderGirl_MVCMovies.Data;
+using CoderGirl_MVCMovies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoderGirl_MVCMovies.ViewModels.Movie
+{
+    public class MovieCreateViewModel
+    {
+        public static MovieCreateViewModel GetMovieCreateViewModel()
+        {
+            List<Director> directors = RepositoryFactory.GetDirectorRepository()
+                .GetModels()
+                .Cast<Director>()
+                .ToList();
+            return new MovieCreateViewModel(directors);
+        }
+
+
+        public string Name { get; set; }
+        public int DirectorId { get; set; }
+        public List<Director> Directors { get; set; }
+        public int Year { get; set; }
+
+        private MovieCreateViewModel(List<Director> directors)
+        {
+            this.Directors = directors;
+        }
+
+        public void Persist()
+        {
+            Models.Movie movie = new Models.Movie
+            {
+                Name = this.Name,
+                DirectorId = this.DirectorId,
+                Year = this.Year
+            };
+            RepositoryFactory.GetMovieRepository().Save(movie);
+        }
+    }
+}
